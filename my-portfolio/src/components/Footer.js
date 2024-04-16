9e4c9c407b68b21a3527dbe85647b986d854c34d
jimhunterangie Hurst  .̲͚͍̬̩͇͙̬̮̹͎͖̘̹͂̓̒̈́̾̈́̾͑́͋͋͋̓ͣ̀ <hangingwithhunter4@gmail.com>
2024-04-16 17:31:58 -0400

    added links to the Footer

--- my-portfolio/src/components/Footer.js
+++ my-portfolio/src/components/Footer.js
@@ -4,9 +4,9 @@ const Footer = () => {
   return (
     <footer>
       <div className="footer-links">
-        <a href="github.com">GitHub</a>
-        <a href="linkedin.com">LinkedIn</a>
-        <a href="stackoverflow.com">Stack Overflow</a>
+        <a href="https://github.com/RRHunterH">GitHub</a>
+        <a href="https://www.linkedin.com/in/hunter-h-8a0655304/">LinkedIn</a>
+        <a href="https://stackoverflow.com/users/24409926/hunter">Stack Overflow</a>
         {/* Add more links as necessary */}
       </div>
     </footer>
