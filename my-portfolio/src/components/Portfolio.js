ab250a095af30302865c6905bda427decf16ea22
jimhunterangie Hurst  .̲͚͍̬̩͇͙̬̮̹͎͖̘̹͂̓̒̈́̾̈́̾͑́͋͋͋̓ͣ̀ <hangingwithhunter4@gmail.com>
2024-04-16 17:11:32 -0400

    6/6 prjects to the portfolio section

--- my-portfolio/src/components/Portfolio.js
+++ my-portfolio/src/components/Portfolio.js
@@ -8,8 +8,8 @@ const Portfolio = () => {
       <div className="background-image"></div>
       <h2 className="title">Portfolio</h2>
       <div className="projects">
-        {/* Project 1 */}
-        <div className="project">
+        {/* Music finder */}
+        <div className="project top-left">
           <h3 className="project-name">Music Finder</h3>
           <img src={process.env.PUBLIC_URL + '/Music Finder.png'} alt="Music Finder" className="project-image" />
           <div className="buttons">
@@ -18,48 +18,75 @@ const Portfolio = () => {
             </a>
             <a href="https://github.com/FisherK19/music-explorer" target="_blank" rel="noopener noreferrer">
               <button className="github-repo-button">GitHub Repo</button>
-            </a>
+            </a> 
           </div>
         </div>
-        {/* Project 2 */}
-        <div className="project">
-          <h3 className="project-name">Project 2</h3>
-          <img src={process.env.PUBLIC_URL + '/placeholder.png'} alt="Project 2" className="project-image" />
+         {/* Snout Route */}
+         <div className="project bottom-right">
+          <h3 className="project-name">Snout Route</h3>
+          <img src={process.env.PUBLIC_URL + '/Snout Route.png'} alt="Project 4" className="project-image" />
           <div className="buttons">
-            <a href="https://example.com" target="_blank" rel="noopener noreferrer">
+            <a href="https://snout-route-60664b49cc33.herokuapp.com/" target="_blank" rel="noopener noreferrer">
               <button className="deployed-app-button">Deployed App</button>
             </a>
-            <a href="https://github.com/example/project2" target="_blank" rel="noopener noreferrer">
+            <a href="https://github.com/JohnChewning/Snout-Route" target="_blank" rel="noopener noreferrer">
               <button className="github-repo-button">GitHub Repo</button>
             </a>
           </div>
         </div>
-        {/* Project 3 */}
-        <div className="project">
-          <h3 className="project-name">Project 3</h3>
-          <img src={process.env.PUBLIC_URL + '/project3.png'} alt="Project 3" className="project-image" />
+        {/* Weather Dashboard */}
+        <div className="project top-right">
+          <h3 className="project-name">Weather Dashboard</h3>
+          <img src={process.env.PUBLIC_URL + 'Weather Dashboard.png'} alt="Project 2" className="project-image" />
           <div className="buttons">
-            <a href="https://example.com" target="_blank" rel="noopener noreferrer">
+            <a href="https://rrhunterh.github.io/Weather-Dashboard/" target="_blank" rel="noopener noreferrer">
               <button className="deployed-app-button">Deployed App</button>
             </a>
-            <a href="https://github.com/example/project3" target="_blank" rel="noopener noreferrer">
+            <a href="https://github.com/RRHunterH/Weather-Dashboard" target="_blank" rel="noopener noreferrer">
               <button className="github-repo-button">GitHub Repo</button>
             </a>
           </div>
         </div>
-        {/* Project 4 */}
-        <div className="project">
-          <h3 className="project-name">Project 4</h3>
-          <img src={process.env.PUBLIC_URL + '/project4.png'} alt="Project 4" className="project-image" />
+        {/* Note Taker */}
+        <div className="project bottom-left">
+          <h3 className="project-name">Note Taker</h3>
+          <img src={process.env.PUBLIC_URL + 'Note Taker.png'} alt="Project 3" className="project-image" />
           <div className="buttons">
-            <a href="https://example.com" target="_blank" rel="noopener noreferrer">
+            <a href="https://note-taker-ninc.onrender.com/" target="_blank" rel="noopener noreferrer">
               <button className="deployed-app-button">Deployed App</button>
             </a>
-            <a href="https://github.com/example/project4" target="_blank" rel="noopener noreferrer">
+            <a href="https://github.com/RRHunterH/Note-Taker" target="_blank" rel="noopener noreferrer">
               <button className="github-repo-button">GitHub Repo</button>
             </a>
           </div>
         </div>
+        {/* Password Generator */}
+        <div className="project bottom-right">
+          <h3 className="project-name">Password Generator</h3>
+          <img src={process.env.PUBLIC_URL + '/Password Generator.png'} alt="Project 4" className="project-image" />
+          <div className="buttons">
+            <a href="https://rrhunterh.github.io/Password-Generator/" target="_blank" rel="noopener noreferrer">
+              <button className="deployed-app-button">Deployed App</button>
+            </a>
+            <a href="https://github.com/RRHunterH/Password-Generator" target="_blank" rel="noopener noreferrer">
+              <button className="github-repo-button">GitHub Repo</button>
+            </a>
+          </div>
+        </div>
+        {/*  Work Day Scheduler */}
+        <div className="project bottom-left">
+          <h3 className="project-name">Work Day Scheduler</h3>
+          <img src={process.env.PUBLIC_URL + '/Work Day Scheduler.png'} alt="Project 3" className="project-image" />
+          <div className="buttons">
+            <a href="https://rrhunterh.github.io/Work-Day-Scheduler/" target="_blank" rel="noopener noreferrer">
+              <button className="deployed-app-button">Deployed App</button>
+            </a>
+            <a href="https://github.com/RRHunterH/Work-Day-Scheduler" target="_blank" rel="noopener noreferrer">
+              <button className="github-repo-button">GitHub Repo</button>
+            </a>
+          </div>
+        </div>
+        {/* Project 6 */}
       </div>
     </section>
   );
