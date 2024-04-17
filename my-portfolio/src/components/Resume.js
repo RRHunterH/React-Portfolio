2d4abca2e20a55261e65dc6d6304ce93c08ddc67
jimhunterangie Hurst  .̲͚͍̬̩͇͙̬̮̹͎͖̘̹͂̓̒̈́̾̈́̾͑́͋͋͋̓ͣ̀ <hangingwithhunter4@gmail.com>
2024-04-17 01:03:49 -0400

    finished resume with all proficiencies

--- my-portfolio/src/components/Resume.js
+++ my-portfolio/src/components/Resume.js
@@ -1,11 +1,36 @@
 import React from 'react';
+import './Resume.css';
 
 const Resume = () => {
   return (
     <section className="resume-container">
+      <div className="resume-background"></div>
       <h2>Resume</h2>
       <div className="resume-download">
         <a href={process.env.PUBLIC_URL + '/Resume.txt'} download>Download Resume</a>
+      </div>
+      <div className="proficiencies">
+        <h3>Front-end Proficiencies</h3>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>React</li>
+          <li>jQuery</li>
+          <li>Bootstrap</li>
+          <li>Tailwind</li>
+        </ul>
+      </div>
+      <div className="proficiencies">
+        <h3>Back-end Proficiencies</h3>
+        <ul>
+          <li>Node.js</li>
+          <li>Express.js</li>
+          <li>MongoDB</li>
+          <li>SQL</li>
+          <li>Sequeliz</li>
+          <li>Regex</li>
+        </ul>
       </div>
     </section>
   );
